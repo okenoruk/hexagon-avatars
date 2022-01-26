d5aa06bd8f431ac6286be951507246b370d7b594
hadi1 <muhammad-11@fst.unair.ac.id>
2022-01-26 19:36:01 +0700

    feat: create set as avatar button

---
+++ scripts/setenv.ts
@@ -0,0 +1,27 @@
+const { writeFile } = require('fs');
+const { argv } = require('yargs');
+// read environment variables from .env file
+require('dotenv').config();
+// read the command line arguments passed with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+const targetPath = isProduction
+  ? `./src/environments/environment.prod.ts`
+  : `./src/environments/environment.ts`;
+// we have access to our environment variables
+// in the process.env object thanks to dotenv
+const environmentFileContent = `
+export const environment = {
+   production: ${isProduction},
+   API_KEY: "${process.env["API_KEY"]}",
+   API_SECRET: "${process.env["API_KEY_SECRET"]}",
+   BEARER_TOKEN: "${process.env["BEARER_TOKEN"]}"
+};
+`;
+// write the content to the respective file
+writeFile(targetPath, environmentFileContent, function (err: any) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Wrote variables to ${targetPath}`);
+});
